46324fb89ab83c4bdebb44cae7c40944b1b42705
PascalPikachu <dingdinginventor@hotmail.com>
2026-09-15 20:47:49 +0800

    Add VR Boxing project

---
+++ VR_Boxing/NidoArena_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_ARROWS = 2290942216U;
+        static const AkUniqueID PLAY_BATTERY = 3086715717U;
+        static const AkUniqueID PLAY_BOOM_CLOSE = 3286261030U;
+        static const AkUniqueID PLAY_BOOM_FAR = 2095317009U;
+        static const AkUniqueID PLAY_BOOM_FLY = 2279870925U;
+        static const AkUniqueID PLAY_CAMERA = 1928751031U;
+        static const AkUniqueID PLAY_CHILD_VOICE = 1319845607U;
+        static const AkUniqueID PLAY_COUNT_DOWN = 3869132978U;
+        static const AkUniqueID PLAY_FALL_DOWN = 3451956096U;
+        static const AkUniqueID PLAY_FLY = 2941622701U;
+        static const AkUniqueID PLAY_FOOD_EJECTOR = 483127359U;
+        static const AkUniqueID PLAY_HURT = 2955259529U;
+        static const AkUniqueID PLAY_JUST_SHOT = 2367183811U;
+        static const AkUniqueID PLAY_MUSIC_GAME_SYSTEM = 775671944U;
+        static const AkUniqueID PLAY_POOL = 1551805620U;
+        static const AkUniqueID PLAY_PUNCH_SOUND = 2792763106U;
+        static const AkUniqueID PLAY_SHOT_ACCUMULATOR = 526875337U;
+        static const AkUniqueID PLAY_START = 2230670290U;
+        static const AkUniqueID PLAY_TARGETDOWN = 175340321U;
+        static const AkUniqueID PLAY_TARGETRISE = 2939892886U;
+        static const AkUniqueID SET_SWITCH_FINISH_GAME = 4260850553U;
+        static const AkUniqueID SET_SWITCH_GAME_MENU = 614894759U;
+        static const AkUniqueID SET_SWITCH_LOSE = 2972225970U;
+        static const AkUniqueID SET_SWITCH_NEW_RECORD = 1152776051U;
+        static const AkUniqueID SET_SWITCH_NORMAL_WIN = 1014040015U;
+        static const AkUniqueID SET_SWITCH_START_GAME = 2627109744U;
+        static const AkUniqueID STOP_FLY = 419831083U;
+        static const AkUniqueID STOP_SYSTEM = 888640723U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GAME_SYSTEM
+        {
+            static const AkUniqueID GROUP = 3129287141U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LOSE_GAME_MENU = 1699838355U;
+                static const AkUniqueID LOSE_MUSIC = 818282622U;
+                static const AkUniqueID START_GAME = 1114964412U;
+                static const AkUniqueID WIN_GAME_MENU = 3696215008U;
+                static const AkUniqueID WIN_MUSIC = 3475929897U;
+            } // namespace SWITCH
+        } // namespace GAME_SYSTEM
+
+        namespace MUSIC_SYSTEM
+        {
+            static const AkUniqueID GROUP = 792781730U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LOSE = 221232726U;
+                static const AkUniqueID NEW_RECORD = 1861161479U;
+                static const AkUniqueID NORMAL_WIN = 941413235U;
+            } // namespace SWITCH
+        } // namespace MUSIC_SYSTEM
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
